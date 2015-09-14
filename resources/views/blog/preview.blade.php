d06fce4778fd9a99cdbf57105645bd090143f993
Lukas Oppermann <lukas@vea.re>
2015-09-14 18:07:50 +0200

    update mobile design

--- resources/views/blog/preview.blade.php
+++ resources/views/blog/preview.blade.php
@@ -1,6 +1,6 @@
 @if ($file['date'] !== false)
 <li class="o-list-item">
-    <span class="c-post-preview-time">{{$file['date']}}</span>
+    <time class="c-post-preview-time">{{$file['date']}}</time>
     <a class="o-link c-post-preview-title" href="{{url('blog/'.$file['link'])}}">
         {{$file['title'] or 'No title provided'}}
     </a>
