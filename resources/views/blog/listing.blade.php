084b00d3955a2a2b20d1f43e6df8a4aed3e1dd7a
Lukas Oppermann <lukas@vea.re>
2015-09-14 18:16:42 +0200

    small style fix

--- resources/views/blog/listing.blade.php
+++ resources/views/blog/listing.blade.php
@@ -4,7 +4,7 @@
         <h1>field notes</h1>
         <p class="c-blog-intro">I am a freelance interaction designer & web developer. I write about design, ux, code and the perfect working environment.</p>
         <p class="c-blog-intro">This blog is a <a href="{{url('blog/-building-a-blog-with-laravel#a-blog-in-progress')}}">work in progress</a>, I am building it as I write, because I don't want to wait with writing until I have the perfect blog coded & designed. Expect dramatic changes and updates. :)</p>
-        <p>If you have comments to any of the articles or suggestions for improvments, please tweet me <a href="https://twitter.com/lukasoppermann">@lukasoppermann</a>.</p>
+        <p class="c-blog-intro">If you have comments to any of the articles or suggestions for improvments, please tweet me <a href="https://twitter.com/lukasoppermann">@lukasoppermann</a>.</p>
     </div>
     <ul class="o-list o-list--none">
         @each('blog.preview', $files, 'file')
