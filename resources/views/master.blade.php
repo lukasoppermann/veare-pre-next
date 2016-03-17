6c3db54700438426093414931f3558edb27fe55e
Lukas Oppermann <lukas@vea.re>
2016-03-17 10:46:52 +0100

    update ga code

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,16 +8,19 @@
         <link href='{{ asset(env("CSS_PATH_PREFIX").elixir('css/app.css')) }}' rel='stylesheet' type='text/css'>
 
         <link href='{{asset(env("CSS_PATH_PREFIX").elixir('css/prism.css'))}}' rel="stylesheet" />
-        <script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        @if (env('APP_ENV') !== 'local')
+            <script>
+              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+              })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+              ga('create', 'UA-7074034-1', 'auto');
+              ga('require', 'linkid');
+              ga('send', 'pageview');
+              ga('set', 'anonymizeIp', true);
 
-          ga('create', 'UA-7074034-1', 'auto');
-          ga('send', 'pageview');
-          ga('set', 'anonymizeIp', true);
-        </script>
+            </script>
+        @endif
     </head>
     <body>
         <div class="o-container">
