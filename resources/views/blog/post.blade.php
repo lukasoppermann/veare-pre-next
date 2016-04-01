3b330807216ce2a0b170c2519daeba95b8d990c5
Lukas Oppermann <lukas@vea.re>
2016-04-01 10:39:05 +0200

    fix

--- resources/views/blog/post.blade.php
+++ resources/views/blog/post.blade.php
@@ -1,6 +1,6 @@
 @extends('master')
 @section('content')
-    <article class="o-article" itemid="{{url('blog/')}}/{!!$link!!}" itemscope itemtype="http://schema.org/BlogPosting">
+    <article class="o-article" itemprop="mainEntityOfPage" itemid="{{url('blog/')}}/{!!$link!!}" itemscope itemtype="http://schema.org/BlogPosting">
         <p><a title="Back to the list of articles" class="o-link o-link--back" href="{{url('blog/')}}">Back to overview</a></p>
 
         <h1 itemprop="headline">{!!$title!!}</h1>
