03c972785bd4136ba9f1c081baa98650deab6652
Lukas Oppermann <lukas@vea.re>
2016-04-01 10:31:12 +0200

    update meta data

--- resources/views/blog/meta.blade.php
+++ resources/views/blog/meta.blade.php
@@ -1,7 +1,7 @@
 <div class="o-article__meta">
     Published
     @if ($author !== false)
-        by <a class="author" itemprop="author" href="http://vea.re" title="about {!!$author!!}" rel="author">{!!$author!!}</a>,
+        by <span itemprop="author" itemscope itemtype="http://schema.org/Person"><a class="author" href="http://vea.re" itemprop="url" title="about {!!$author!!}" rel="author"><span itemprop="name">{!!$author!!}</span></a></span>,
     @endif
     <time class="article_time" itemprop="datePublished" datetime="{{$machine_date}}">{!!$date!!}</time> •
     <time datetime="{!!$readingTime!!}m">{!!$readingTime!!} min read</time>
