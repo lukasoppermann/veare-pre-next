b36eaad15bb704e4941fb7f9417976cac9be98ba
Lukas Oppermann <lukas@vea.re>
2016-04-01 12:01:52 +0200

    fix

--- resources/views/blog/meta.blade.php
+++ resources/views/blog/meta.blade.php
@@ -1,7 +1,7 @@
 <div class="o-article__meta">
     Published
     @if ($author !== false)
-        by <span itemprop="author" itemscope itemtype="http://schema.org/Person"><a class="author" href="https://twitter.com/lukasoppermann" itemprop="url" title="about {!!$author!!}" rel="author"><span itemprop="name">{!!$author!!}</span></a></span> / <a itemprop="publisher" itemprop="http://schema.org/Organization" itemscope href="http://vea.re"><span itemtype="name">vea.re</span></span>,
+        by <span itemprop="author" itemscope itemtype="http://schema.org/Person"><a class="author" href="https://twitter.com/lukasoppermann" itemprop="url" title="about {!!$author!!}" rel="author"><span itemprop="name">{!!$author!!}</span></a></span> / <a itemprop="publisher" itemprop="http://schema.org/Organization" itemscope href="http://vea.re"><span itemtype="name">vea.re</span></a>,
     @endif
     <time class="article_time" itemprop="datePublished" datetime="{{$machine_date}}">{!!$date!!}</time> •
     <time datetime="{!!$readingTime!!}m">{!!$readingTime!!} min read</time>
