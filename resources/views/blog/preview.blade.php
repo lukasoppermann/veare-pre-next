3baa7cf040062917047276a777f256c9d0a5848c
Lukas Oppermann <lukas@vea.re>
2016-04-02 14:37:21 +0200

    small fixes

--- resources/views/blog/preview.blade.php
+++ resources/views/blog/preview.blade.php
@@ -5,10 +5,11 @@
         <a href="" class="o-article-preview__category o-article-preview__category--blue" date-category="{{$post['meta']['category']}}">{{$post['meta']['category']}}</a>
     </div>
     <a itemprop="url" class="o-article-preview__link" title="Read: {{$post['title'] or 'No title provided'}}" href="{{url('blog/'.$post['link'])}}">
+    <link itemprop="mainEntityOfPage" href="{{url('blog/'.$post['link'])}}" />
         @if( $post['meta']['series'] !== false )
             <h4 class="o-article-preview__series">{{$post['meta']['series']['name']}} – part {{$post['meta']['series']['part']}}:</h4>
         @endif
-        <h3 class="o-article-preview__title" itemprop="name">{{$post['title'] or 'No title provided'}}</h3>
+        <h3 class="o-article-preview__title" itemprop="name headline">{{$post['title'] or 'No title provided'}}</h3>
         <p class="o-article-preview__excerpt" itemprop="description">{{$post['meta']['preview']}}</p>
     </a>
 </li>
