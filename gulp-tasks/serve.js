650afa1bee05876e1caf6300a497ea50b715d273
Lukas Oppermann <lukas@vea.re>
2017-09-05 19:38:14 +0200

    fix issue when server already running

--- gulp-tasks/serve.js
+++ gulp-tasks/serve.js
@@ -4,27 +4,29 @@
  *
  */
 module.exports.serve = () => {
-  const forever = require('gulp-forever-monitor')
   const pack = require('../package.json')
   const exec = require('child_process').execSync
+  const forever = require('gulp-forever-monitor')
 
   return () => {
-
-    exec('pkill node')
-
+    try {
+      exec('pkill node')
+    } catch (err) {
+      // console.log(err)
+    }
     var foreverMonitorOptions = {
       env: process.env,
       args: process.argv,
       watch: true,
-      watchIgnorePatterns: ['node_modules/**', 'public/**']
+      watchIgnorePatterns: ['node_modules/**', '!public/**']
     }
 
     forever(pack.main, foreverMonitorOptions)
-    .on('watch:restart', function(fileInfo) {
-      console.log('server was restarted');
+    .on('watch:restart', function (fileInfo) {
+      console.log('server was restarted')
     })
-    .on('exit', function() {
-      console.log('server was closed');
+    .on('exit', function () {
+      console.log('server was closed')
     })
   }
 }
