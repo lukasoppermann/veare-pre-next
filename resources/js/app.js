0be0c0fe2579968370579f518bdb7db5f8c5e423
Lukas Oppermann <lukas@vea.re>
2017-11-18 17:04:04 +0100

    menu component with basic animations

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,3 @@
-import { Menu } from './modules/menu'
-
-const menu = new Menu('.o-nav')
-menu.transitionOnScroll()
-
-document.querySelector('.o-nav__icon').addEventListener('click', function () {
-  document.body.classList.toggle('is-active--overlay-menu')
-  this.classList.toggle('is-active')
-})
-
 Array.prototype.forEach.call(document.querySelectorAll('pre > code'), function (item) {
   item.classList.add('line-numbers')
   item.parentElement.setAttribute('columns', item.getAttribute('columns'))
--- resources/js/modules/responsive-menu.ts
+++ resources/js/modules/responsive-menu.ts
@@ -1,6 +1,8 @@
 /* global HTMLElement CustomEvent */
 'use strict'
 
+// import { shape, timeline, render, play } from 'wilderness'
+
 declare const ShadyCSS // eslint-disable-line no-unused-vars
 let template = document.createElement('template')
 template.innerHTML = `<style>
@@ -33,6 +35,7 @@ template.innerHTML = `<style>
   nav{
     display: flex;
     position: absolute;
+    z-index: 10;
     height: 66px;
     top: -66px;
     right: 0;
@@ -44,31 +47,161 @@ template.innerHTML = `<style>
     top: 0;
     opacity: 1;
   }
-  .o-nav.is-hidden .o-nav__icon{
-    display: flex;
-    top: 30px;
-    padding-right: 10px;
-  }
-  .o-nav__icon svg {
+  #menuIcon {
+    position: absolute;
+    top: 0;
+    right: 0;
+    z-index: 100;
+    pointer-events: all;
+    cursor: pointer;
     width: 60px;
     height: 60px;
     transform: translate3d(4px, 0, 0);
   }
+  #menuIcon path, :host([extended][overlayvisible]) #menuIcon path{
+    fill: none;
+    stroke-width: 40px;
+    stroke: var(--responsive-menu-menu-icon, rgb(33,37,41));
+    stroke-dashoffset: 0;
+    opacity: 1;
+    transition: opacity .5s .2s ease, stroke-dashoffset .5s .2s ease, stroke .3s ease;
+  }
+  :host([extended]) #menuIcon path {
+    transition: stroke-dashoffset 0.5s cubic-bezier(0.25, -0.25, 0.75, 1.25), stroke-dasharray 0.5s cubic-bezier(0.25, -0.25, 0.75, 1.25), stroke .3s ease;
+    stroke-dashoffset: 240px;
+    opacity: 0;
+  }
+  #menuIcon path#top,
+  #menuIcon path#bottom {
+    stroke-dasharray: 270px 950px;
+  }
+  #menuIcon path#middle {
+    stroke-dasharray: 270px 270px;
+  }
+  :host([overlayVisible]) #menuIcon path#top,
+  :host([overlayVisible]) #menuIcon path#bottom {
+    stroke-dashoffset: -666px;
+  }
+  :host([overlayVisible]) #menuIcon path#middle {
+    stroke-dashoffset: -270px;
+  }
+  :host([overlayVisible]) #menuIcon path{
+    stroke: var(--responsive-menu-close-icon, rgb(255,255,255));
+  }
+  :host([overlayVisible]){
+    height: 100%;
+  }
+  :host([overlayVisible]) nav{
+    flex-direction: column;
+    height: 100%;
+    width: 30%;
+    min-width: 300px;
+    color: rgb(255,255,255);
+    opacity: 0;
+    padding-top: var(--size-xl);
+  }
+  :host(.is-active) nav{
+    opacity: 1;
+    top: 0;
+  }
+  /*Firefox bug: responsive-menu > [slot="items"]*/
+  :host([overlayVisible]) nav [slot="items"],
+  :host([overlayVisible]) ::slotted([slot="items"]){
+    font-size: var(--size-xl);
+  }
   #info{
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    pointer-events: none;
+    position: absolute;
+    z-index: 10;
+    opacity: 0;
+    bottom: -50px;
+    left: 0;
+    transition: bottom .3s ease, opacity .3s ease;
+    width: 70%;
+    height: 100%;
+    max-width: calc(100% - 300px);
+    background: rgb(255,255,255);
+  }
+  :host([overlayVisible]) #info{
+    opacity: 0;
+    bottom: -50px;
+    pointer-events: all;
+    transition: bottom .3s ease, opacity .3s ease;
+  }
+  :host(.is-active) #info{
+    opacity: 1;
+    bottom: 0px;
+    transition: bottom .3s ease, opacity .3s ease;
+  }
+  @media(max-width: 700px){
+    :host([overlayVisible]) #info{
+      display: none;
+    }
+  }
+  #background{
+    z-index: 1;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: transparent;
+    transition: background-color .3s ease;
+  }
+  #background.is-active{
+    background-color: var(--blue);
+  }
+  footer{
     display: none;
+    pointer-events: all;
+  }
+  :host([overlayVisible]) footer{
+    display: block;
+    position: absolute;
+    right: 0;
+    bottom: -50px;
+    z-index: 10;
+    height: 50px;
+    width: 30%;
+    min-width: 300px;
+    opacity: 0;
+    transition: bottom .3s ease, opacity .3s ease;
+  }
+  :host(.is-active) footer{
+    bottom: 0;
+    opacity: 1;
   }
   </style>
+  <svg id="menuIcon" viewBox="0 0 800 500">
+    <path d="M270,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200" id="top"></path>
+    <path d="M270,320 L540,320" id="middle"></path>
+    <path d="M270,210 C300,210 520,210 540,210 C740,210 640,530 520,410 C440,330 300,190 300,190" id="bottom" transform="translate(480, 320) scale(1, -1) translate(-480, -318) "></path>
+  </svg>
   <div id="info">
     <slot name="info"></slot>
+    <slot name="infoBackground"></slot>
   </div>
   <nav>
     <slot name="items"></slot>
+    <slot name="navBackground"></slot>
   </nav>
+  <footer>
+    <slot name="footer"></slot>
+  </footer>
+  <div id="background">
+    <slot name="background"></slot>
+  </div>
   `
 
 class ResponsiveMenu extends HTMLElement { // eslint-disable-line no-unused-vars
   /* Typescript: declare variables */
   private _thresholdY: number = 150 // eslint-disable-line no-undef
+  private _collpaseSize: number = 700 // eslint-disable-line no-undef
+  private _animateOverlayBg: boolean = true // eslint-disable-line no-undef
+  private _hideOverlayDelay: number = 300 // eslint-disable-line no-undef
 
   constructor () {
     // If you define a constructor, always call super() first!
@@ -90,7 +223,7 @@ class ResponsiveMenu extends HTMLElement { // eslint-disable-line no-unused-vars
   * @description return attributes that should be watched for updates
    */
   static get observedAttributes () {
-    return []
+    return ['thresholdY', 'collpaseSize', 'hideOverlayDelay', 'animateOverlayBg']
   }
   /**
   * @method observedAttributes
@@ -106,9 +239,23 @@ class ResponsiveMenu extends HTMLElement { // eslint-disable-line no-unused-vars
    */
   connectedCallback () {
     // initial run
-    this.toggleClassOnY(window.pageYOffset)
+    this.toggleExtended()
     // onScrollEvent
-    this.transitionOnScroll()
+    let scrollEnded
+    document.addEventListener('scroll', this.throttle(function () {
+      this.toggleExtended()
+      // scrollEnded
+      clearTimeout(scrollEnded)
+      scrollEnded = setTimeout(() => {
+        this.toggleExtended()
+      }, 300)
+    }.bind(this), 20))
+    // onResizeEvent
+    window.addEventListener('resize', this.throttle(function () {
+      this.toggleExtended()
+    }.bind(this), 20))
+    // menuIcon on click
+    this.shadowRoot.querySelector('#menuIcon').addEventListener('click', this.toggleOverlay.bind(this))
   }
   /**
   * @method throttle
@@ -127,29 +274,104 @@ class ResponsiveMenu extends HTMLElement { // eslint-disable-line no-unused-vars
     }
   }
   /**
-  * @method transitionOnScroll
-  * @description transitioning the menu when scrolling past a specified point
+  * @method toggleExtendedOnY
+  * @description toggle extended attribute when scrollPosY is bigger than this._thresholdY
    */
-  transitionOnScroll () {
-    let posY = window.pageYOffset
-
-    document.addEventListener('scroll', this.throttle(function () {
-
-      if(Math.abs(posY - window.pageYOffset) > 4) {
-        this.toggleClassOnY(window.pageYOffset)
-      }
-
-    }.bind(this), 20))
-  }
-  /**
-  * @method toggleClassOnY
-  * @description toggle class when scrollPosY is bigger than this._thresholdY
-   */
-  toggleClassOnY (posY) {
-    if (posY > this._thresholdY) {
+  toggleExtended () {
+    if (window.pageYOffset > this._thresholdY) {
+      return this.removeAttribute('extended')
+    }
+    if(document.documentElement.clientWidth < this.collpaseSize) {
       return this.removeAttribute('extended')
     }
     this.setAttribute('extended', '')
+  }
+  /**
+  * @method setter thresholdY
+  * @description set the thresholdY property
+   */
+  set thresholdY (thresholdY: number) {
+    if (this._thresholdY === thresholdY) return
+    this._thresholdY = thresholdY
+  }
+  /**
+   * @method getter thresholdY
+   * @description get the thresholdY property
+   */
+  get thresholdY () {
+    return this._thresholdY
+  }
+  /**
+  * @method setter collpaseSize
+  * @description set the collpaseSize property
+   */
+  set collpaseSize (collpaseSize: number) {
+    if (this._collpaseSize === collpaseSize) return
+    this._collpaseSize = collpaseSize
+  }
+  /**
+   * @method getter collpaseSize
+   * @description get the collpaseSize property
+   */
+  get collpaseSize () {
+    return this._collpaseSize
+  }
+  /**
+  * @method setter hideOverlayDelay
+  * @description set the hideOverlayDelay property
+   */
+  set hideOverlayDelay (hideOverlayDelay: number) {
+    if (this._hideOverlayDelay === hideOverlayDelay) return
+    this._hideOverlayDelay = hideOverlayDelay
+  }
+  /**
+   * @method getter hideOverlayDelay
+   * @description get the hideOverlayDelay property
+   */
+  get hideOverlayDelay () {
+    return this._hideOverlayDelay
+  }
+  /**
+  * @method setter animateOverlayBg
+  * @description set the animateOverlayBg property
+   */
+  set animateOverlayBg (animateOverlayBg: boolean) {
+    if (this._animateOverlayBg === animateOverlayBg) return
+    this._animateOverlayBg = animateOverlayBg
+  }
+  /**
+   * @method getter animateOverlayBg
+   * @description get the animateOverlayBg property
+   */
+  get animateOverlayBg () {
+    return this._animateOverlayBg
+  }
+  /**
+   * @method toggleOverlay
+   * @description everything tha happens when toggling the overlay
+   */
+  toggleOverlay () {
+    // show overlay
+    if (! this.hasAttribute('overlayVisible')) {
+      this.dispatchEvent(new CustomEvent('toggleOverlay', { detail: { visible: true } } ))
+      document.body.style.overflow = 'hidden'
+      if (this._animateOverlayBg === true) {
+        this.shadowRoot.querySelector('#background').classList.add('is-active')
+      }
+      this.classList.add('is-active')
+      return this.setAttribute('overlayVisible', '')
+    }
+    // hide overlay
+    this.dispatchEvent(new CustomEvent('toggleOverlay', { detail: { visible: false } } ))
+    if (this._animateOverlayBg === true) {
+      this.shadowRoot.querySelector('#background').classList.remove('is-active')
+    }
+    this.classList.remove('is-active')
+
+    setTimeout(() => {
+      this.removeAttribute('overlayVisible')
+      document.body.style.overflow = 'auto'
+    }, this.hideOverlayDelay)
   }
 }
 
