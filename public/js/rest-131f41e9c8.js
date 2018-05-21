870bd7dd3f78e84b3831846f08d85c0f5c0289f9
Lukas Oppermann <lukas@vea.re>
2018-05-21 19:33:59 +0200

    build

--- public/js/rest-131f41e9c8.js
+++ public/js/rest-131f41e9c8.js
@@ -1,2 +1,2 @@
 !function(){"use strict";var e,a,n,t,o,s;(l=>{"localhost"!==l.location.hostname&&(e=l,a=document,n="script",t="ga",e.GoogleAnalyticsObject=t,e.ga=e.ga||function(){(e.ga.q=e.ga.q||[]).push(arguments)},e.ga.l=1*new Date,o=a.createElement(n),s=a.getElementsByTagName(n)[0],o.async=1,o.src="https://www.google-analytics.com/analytics.js",s.parentNode.insertBefore(o,s),ga("create","UA-7074034-1","auto",{allowLinker:!0}),ga("require","linker"),ga("linker:autoLink",["veare.de","lukasoppermann.com","lukasoppermann.de","lukas-oppermann.de"]),ga("set","anonymizeIp",!0),ga("send","pageview"))})(window)}();
-//# sourceMappingURL=/rest.js.map
+//# sourceMappingURL=rest.js.map
