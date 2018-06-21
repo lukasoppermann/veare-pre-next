223867b881772bd139cec851a96a21c49326480c
Lukas Oppermann <lukas@vea.re>
2018-06-22 00:46:48 +0200

    add testing environment config

--- app/config/contentful.js
+++ app/config/contentful.js
@@ -5,12 +5,14 @@ module.exports = {
   space: process.env.CONTENTFUL_SPACE,
   accessToken: {
     dev: process.env.CONTENTFUL_TOKEN_PREVIEW,
-    production: process.env.CONTENTFUL_TOKEN
+    production: process.env.CONTENTFUL_TOKEN,
+    testing: process.env.CONTENTFUL_TOKEN
   },
   webhookUser: process.env.CONTENTFUL_HOOK_USER,
   webhookPassword: process.env.CONTENTFUL_HOOK_PW,
   host: {
     dev: 'preview.contentful.com',
-    production: 'cdn.contentful.com'
+    production: 'cdn.contentful.com',
+    testing: 'cdn.contentful.com'
   }
 }
