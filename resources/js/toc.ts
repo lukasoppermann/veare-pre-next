13fa028f2021bf13392264bd48a1bcd6db21420b
Lukas Oppermann <lukas@vea.re>
2018-07-02 13:48:58 +0200

    adjust for mobile views

---
+++ resources/js/toc.ts
@@ -0,0 +1,36 @@
+document.querySelectorAll('[data-toc]').forEach((toc) => {
+  let sectionsContainer = toc.closest('page-sections')
+  toc.querySelectorAll('a.Toc__chapter__link').forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault()
+      sectionsContainer.goTo(link.getAttribute('href').substr(1))
+    })
+  })
+})
+
+if (document.querySelector('body').classList.contains('Page--work')) {
+  let paginationContainer = document.querySelector('.Project__pagination-container')
+  let paginationItems = paginationContainer.querySelectorAll('.Pagination__item')
+  let pageSections = document.querySelector('page-sections')
+  let toc = document.querySelector('.Project__toc')
+  let tocOffset = toc.getBoundingClientRect().top + (window.pageYOffset || document.documentElement.scrollTop)
+  let padding = window.getComputedStyle(pageSections).getPropertyValue('--outer-padding');
+  let footerHeight = window.getComputedStyle(document.querySelector('.Footer-Section')).getPropertyValue('height')
+  paginationContainer.style.top = `${tocOffset}px`
+  paginationContainer.style.left = padding
+  paginationContainer.style.height = `calc(100% - ${tocOffset}px - ${footerHeight} - 200px)`
+  pageSections.addEventListener('activateSection', (event) => {
+    paginationItems.forEach((item) => {
+      if (item.querySelector('a').getAttribute('href').substr(1) === event.detail.sectionName) {
+        item.classList.add('active')
+      } else {
+        item.classList.remove('active')
+      }
+    })
+  })
+  paginationItems.forEach((item) => {
+    item.addEventListener('click', (event) => {
+      pageSections.goTo(item.querySelector('a').getAttribute('href').substr(1))
+    })
+  })
+}
