d3aa4d54b1db1ea9ab27fd35fb7b3e53c5c02426
Lukas Oppermann <lukas@vea.re>
2018-07-02 14:33:18 +0200

    add resize event

--- resources/js/toc.ts
+++ resources/js/toc.ts
@@ -1,3 +1,5 @@
+import {default as throttle} from './modules/throttle'
+
 document.querySelectorAll('[data-toc]').forEach((toc) => {
   let sectionsContainer = toc.closest('page-sections')
   toc.querySelectorAll('a.Toc__chapter__link').forEach((link) => {
@@ -9,28 +11,33 @@ document.querySelectorAll('[data-toc]').forEach((toc) => {
 })
 
 if (document.querySelector('body').classList.contains('Page--work')) {
-  let paginationContainer = document.querySelector('.Project__pagination-container')
-  let paginationItems = paginationContainer.querySelectorAll('.Pagination__item')
-  let pageSections = document.querySelector('page-sections')
-  let toc = document.querySelector('.Project__toc')
-  let tocOffset = toc.getBoundingClientRect().top + (window.pageYOffset || document.documentElement.scrollTop)
-  let padding = window.getComputedStyle(pageSections).getPropertyValue('--outer-padding');
-  let footerHeight = window.getComputedStyle(document.querySelector('.Footer-Section')).getPropertyValue('height')
-  paginationContainer.style.top = `${tocOffset}px`
-  paginationContainer.style.left = padding
-  paginationContainer.style.height = `calc(100% - ${tocOffset}px - ${footerHeight} - 200px)`
-  pageSections.addEventListener('activateSection', (event) => {
+  let paginationInit = throttle(() => {
+    let paginationContainer = document.querySelector('.Project__pagination-container')
+    let paginationItems = paginationContainer.querySelectorAll('.Pagination__item')
+    let pageSections = document.querySelector('page-sections')
+    let toc = document.querySelector('.Project__toc')
+    let tocOffset = toc.getBoundingClientRect().top + (window.pageYOffset || document.documentElement.scrollTop)
+    let padding = window.getComputedStyle(pageSections).getPropertyValue('--outer-padding');
+    let footerHeight = window.getComputedStyle(document.querySelector('.Footer-Section')).getPropertyValue('height')
+    paginationContainer.style.top = `${tocOffset}px`
+    paginationContainer.style.left = padding
+    paginationContainer.style.height = `calc(100% - ${tocOffset}px - ${footerHeight} - 200px)`
+    pageSections.addEventListener('activateSection', (event) => {
+      paginationItems.forEach((item) => {
+        if (item.querySelector('a').getAttribute('href').substr(1) === event.detail.sectionName) {
+          item.classList.add('active')
+        } else {
+          item.classList.remove('active')
+        }
+      })
+    })
     paginationItems.forEach((item) => {
-      if (item.querySelector('a').getAttribute('href').substr(1) === event.detail.sectionName) {
-        item.classList.add('active')
-      } else {
-        item.classList.remove('active')
-      }
+      item.addEventListener('click', (event) => {
+        pageSections.goTo(item.querySelector('a').getAttribute('href').substr(1))
+      })
     })
-  })
-  paginationItems.forEach((item) => {
-    item.addEventListener('click', (event) => {
-      pageSections.goTo(item.querySelector('a').getAttribute('href').substr(1))
-    })
-  })
+  }, 100)
+  
+  paginationInit()
+  window.addEventListener('resize', paginationInit)
 }
