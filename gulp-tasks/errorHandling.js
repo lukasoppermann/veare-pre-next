d4102aa6bd6aac1c4e93907a541e0ebc2ea44077
Lukas Oppermann <lukas@vea.re>
2018-10-09 22:07:41 +0200

    removing unneeded deps

--- gulp-tasks/errorHandling.js
+++ gulp-tasks/errorHandling.js
@@ -1,6 +1,6 @@
 module.exports = (errordata) => {
   return function error (errordata) {
-    require('gulp-util').log(require('chalk').white.bgRed.bold(`ERROR:`) + ` ${errordata.toString()}`)
+    require('gulp-util').log(`ERROR: ${errordata.toString()}`)
     this.emit('end')
   }
 }
