c090bb1de1e172d224633dd2d1f35a847990365d
Lukas Oppermann <lukas@vea.re>
2018-11-19 12:22:28 +0100

    remove resolution from name

--- app/transformer/AssetTransformer.js
+++ app/transformer/AssetTransformer.js
@@ -4,11 +4,6 @@ const Transformer = require('./Transformer')
 
 class AssetTransformer extends Transformer {
   transform (data) {
-    // // abort if no header
-    // if (typeof data.fields !== 'object') {
-    //   return null
-    // }
-    // get file
     let file = this.getContent(data, 'file')
 
     return {
@@ -21,23 +16,9 @@ class AssetTransformer extends Transformer {
         url: file.url,
         fileName: file.fileName,
         details: file.details,
-        contentType: file.contentType,
-        resolution: this.getResolution(file.fileName)
+        contentType: file.contentType
       }
     }
-  }
-  /**
-   * Extract resolution from filename
-   */
-  getResolution (file, defaultResolution = 1) {
-    let fileName = file.split('.').slice(0, -1)[0]
-    let fileResolution = parseInt(fileName.substring(fileName.lastIndexOf('@') + 1, fileName.lastIndexOf('@') + 2))
-
-    if (Number.isInteger(fileResolution)) {
-      return fileResolution
-    }
-
-    return defaultResolution
   }
 }
 
