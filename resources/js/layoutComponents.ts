cc8adb89b7e4206d0b48ba1aee38f464df266d9a
Lukas Oppermann <lukas@vea.re>
2018-11-20 15:30:07 +0100

    fixing grid

--- resources/js/layoutComponents.ts
+++ resources/js/layoutComponents.ts
@@ -1,6 +1,7 @@
 import '../../node_modules/@nuclei-components/page-sections/dist/page-sections.js'
 import '../../node_modules/@nuclei-components/grid/dist/grid.js'
 import '../../node_modules/@nuclei-components/responsive-image/dist/responsiveImage.js'
+import '../../node_modules/@nuclei-components/lazy-picture/dist/lazyPicture.js'
 
 window.nucleiGrid = {
   breakpoints: {
