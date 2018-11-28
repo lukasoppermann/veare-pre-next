5c92554b521cefa7a85119deb497a2531f384527
Lukas Oppermann <lukas@vea.re>
2018-11-28 14:07:52 +0100

    fixing most standard issues

--- resources/js/index.ts
+++ resources/js/index.ts
@@ -1,61 +1,61 @@
-/* global fetchInject*/
-import {default as webComponentsSupported} from './modules/wcSupported'
+/* global fetchInject fetch */
+import webComponentsSupported from './modules/wcSupported'
 // get correct file name
 const revisionedFiles = fetch('/revisionedFiles').then(response => {
   return response.json()
 })
-const baseUrl = window.location.protocol+'//'+window.location.host
+const baseUrl = `${window.location.protocol}//${window.location.host}`
 // get fetchInject class
 const scriptPromise = new Promise((resolve, reject) => {
-  const script = window.document.createElement('script');
-  window.document.head.appendChild(script);
-  script.onload = resolve;
-  script.onerror = reject;
-  script.async = true;
-  script.src = baseUrl + '/js/fetch-inject.min.js?v=1.9.1';
+  const script = window.document.createElement('script')
+  window.document.head.appendChild(script)
+  script.onload = resolve
+  script.onerror = reject
+  script.async = true
+  script.src = baseUrl + '/js/fetch-inject.min.js?v=1.9.1'
 })
 
 Promise.all([revisionedFiles, scriptPromise])
-.then(results => {
-  let json = results[0]
-  // make sure WC are working
-  const webComponentsAvailable = new Promise((resolve) => {
-    if (!webComponentsSupported()) {
-      return fetchInject([
-        // polyfill
-        `${baseUrl}/js/webcomponents-sd-ce.js`,
-        'https://polyfill.io/v2/polyfill.min.js?features=IntersectionObserver'
-      ]).then(() => {
-        resolve()
+  .then(results => {
+    let json = results[0]
+    // make sure WC are working
+    const webComponentsAvailable = new Promise((resolve) => {
+      if (!webComponentsSupported()) {
+        return fetchInject([
+          // polyfill
+          `${baseUrl}/js/webcomponents-sd-ce.js`,
+          'https://polyfill.io/v2/polyfill.min.js?features=IntersectionObserver'
+        ]).then(() => {
+          resolve()
+        })
+      }
+      resolve()
+    })
+    // load critical layout components
+    const layoutComponents = fetchInject([`${baseUrl}/${json['js/layoutComponents.js']}`], webComponentsAvailable)
+    let body = document.querySelector('body')
+    // load webfont and view intro once downloaded
+    fetchInject([
+      `https://fonts.googleapis.com/css?family=Montserrat:700|Noto+Serif:400,400i|Source+Sans+Pro:400,600`
+    ], layoutComponents)
+      .then(() => {
+        if (body !== null) {
+          body.style.opacity = '1'
+        }
+      }, () => {
+        if (body !== null) {
+          body.style.opacity = '1'
+        }
       })
-    }
-    resolve()
-  })
-  // load critical layout components
-  const layoutComponents = fetchInject([`${baseUrl}/${json['js/layoutComponents.js']}`], webComponentsAvailable)
-  let body = document.querySelector('body')
-  // load webfont and view intro once downloaded
-  fetchInject([
-    `https://fonts.googleapis.com/css?family=Montserrat:700|Noto+Serif:400,400i|Source+Sans+Pro:400,600`
-  ], layoutComponents)
-  .then(() => {
-    if(body !== null) {
-      body.style.opacity = "1"
-    }
-  }, () => {
-    if(body !== null) {
-      body.style.opacity = "1"
-    }
-  })
 
-  // load responsiveMenu
-  fetchInject([
-    `${baseUrl}/${json['js/responsiveMenu.js']}`,
-    `${baseUrl}/${json['js/nucleiButton.js']}`,
-    `${baseUrl}/${json['js/toc.js']}`,
-    `${baseUrl}/${json['js/rest.js']}`
-  ], webComponentsAvailable)
-  .then(() => {
-    document.querySelector('responsive-menu').style.display = "block"
+    // load responsiveMenu
+    fetchInject([
+      `${baseUrl}/${json['js/responsiveMenu.js']}`,
+      `${baseUrl}/${json['js/nucleiButton.js']}`,
+      `${baseUrl}/${json['js/toc.js']}`,
+      `${baseUrl}/${json['js/rest.js']}`
+    ], webComponentsAvailable)
+      .then(() => {
+        document.querySelector('responsive-menu').style.display = 'block'
+      })
   })
-})
--- resources/js/toc.ts
+++ resources/js/toc.ts
@@ -1,4 +1,4 @@
-import {default as throttle} from './modules/throttle'
+import throttle from './modules/throttle'
 
 document.querySelectorAll('[data-toc]').forEach((toc) => {
   let sectionsContainer = toc.closest('page-sections')
@@ -17,7 +17,7 @@ if (document.querySelector('body').classList.contains('Page--work')) {
     let pageSections = document.querySelector('page-sections')
     let toc = document.querySelector('.Project__toc')
     let tocOffset = toc.getBoundingClientRect().top + (window.pageYOffset || document.documentElement.scrollTop)
-    let padding = window.getComputedStyle(pageSections).getPropertyValue('--outer-padding');
+    let padding = window.getComputedStyle(pageSections).getPropertyValue('--outer-padding')
     let footerHeight = window.getComputedStyle(document.querySelector('.Footer-Section')).getPropertyValue('height')
     paginationContainer.style.top = `${tocOffset}px`
     paginationContainer.style.left = padding
