45fb5a5d1315d997546e6456217c04838e2a4d49
Lukas Oppermann <lukas@vea.re>
2019-01-01 17:42:40 +0100

    fix

---
+++ app/transformer/ProjectInfoTransformer.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const Transformer = require('./Transformer')
+const convertMarkdown = require('../services/convertMarkdown')
+
+class ProjectInfoTransformer extends Transformer {
+  transform (data) {
+    return {
+      id: data.sys.id,
+      createdAt: data.sys.createdAt,
+      updatedAt: data.sys.updatedAt,
+      fields: {
+        type: data.sys.contentType.sys.id,
+        facts: this.getContent(data, 'facts'),
+        tableOfContent: this.getContent(data, 'tableOfContent'),
+        challenge: convertMarkdown(this.getContent(data, 'challenge'))
+      }
+    }
+  }
+}
+
+module.exports = ProjectInfoTransformer
