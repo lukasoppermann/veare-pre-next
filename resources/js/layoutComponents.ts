088a133876b86417fb24771deb63511190491a8b
Lukas Oppermann <lukas@vea.re>
2019-09-27 22:54:18 +0200

    remove nucleiButton webcomponent

--- resources/js/layoutComponents.ts
+++ resources/js/layoutComponents.ts
@@ -1,6 +1,5 @@
 import '../../node_modules/@nuclei-components/page-sections/dist/page-sections.js'
 import '../../node_modules/@nuclei-components/lazy-picture/dist/lazyPicture.js'
-import './modules/nucleiButton'
 import webComponentsSupported from './modules/webComponentsSupported'
 
 window.webComponentsSupported = webComponentsSupported
