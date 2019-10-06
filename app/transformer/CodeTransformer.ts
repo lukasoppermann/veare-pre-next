fc44f4eba7552e342c98da3c2842d7770bd6b6e0
Lukas Oppermann <lukas@vea.re>
2019-10-07 00:24:24 +0200

    repalce prismjs with highlight.js

--- app/transformer/CodeTransformer.ts
+++ app/transformer/CodeTransformer.ts
@@ -1,10 +1,12 @@
 'use strict'
 
 const Transformer = require('./Transformer')
-const syntaxHighlight = require('../services/syntaxHighlight')
+import hljs = require('highlight.js')
 
 class CodeTransformer extends Transformer {
   transform (data) {
+    const programmingLanguage = this.getContent(data, 'language', 'none').toLowerCase()
+
     return {
       id: data.sys.id,
       createdAt: data.sys.createdAt,
@@ -12,8 +14,8 @@ class CodeTransformer extends Transformer {
       fields: {
         type: data.sys.contentType.sys.id,
         fileOrPath: this.getContent(data, 'fileOrPath'),
-        code: syntaxHighlight(this.getContent(data, 'code'), this.getContent(data, 'language', 'none').toLowerCase()),
-        language: this.getContent(data, 'language', 'none').toLowerCase()
+        code: hljs.highlight(programmingLanguage, this.getContent(data, 'code')).value,
+        language: programmingLanguage
       }
     }
   }
