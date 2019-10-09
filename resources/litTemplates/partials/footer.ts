760aa5f85280589491f7a0c4c507d6a9721aac4f
Lukas Oppermann <lukas@vea.re>
2019-10-09 23:21:39 +0200

    add new sass files for lit Templates to build flow

--- resources/litTemplates/partials/footer.ts
+++ resources/litTemplates/partials/footer.ts
@@ -2,7 +2,6 @@ const { html } = require('@popeindustries/lit-html-server')
 
 export default html`
   <footer>
-  <link type="text/css" href="/css/footer.css" rel="stylesheet" />
   Lukas
   </footer>
 `
