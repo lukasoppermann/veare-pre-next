d4f81f7a753a6e3b842ff415d9ecfe78b35eaf7b
Lukas Oppermann <lukas@vea.re>
2019-10-11 15:29:21 +0200

    working on footer content

--- resources/litTemplates/partials/footer.ts
+++ resources/litTemplates/partials/footer.ts
@@ -3,7 +3,27 @@ const fs = require('fs')
 
 export default html`
 <footer class="Footer Grid">
-  Lukas Oppermann
+  <div class="Footer__bio">
+    <h4>Lukas Oppermann</h4>
+    Creative Director &<br />
+    Lead UI/UX Designer
+  </div>
+  <div class="Footer__contact">
+    <h4>Contact</h4>
+    lukas@vea.re<br />
+  </div>
+  <div class="Footer__connect">
+    <h4>Connect</h4>
+    dribbble<br />
+    linkedIn<br />
+    twitter<br />
+    github
+  </div>
+  <div class="Footer__legal">
+    <h4>Legal</h4>
+    Imprint<br />
+    Privacy policy
+  </div>
   <hr />
   ${fs.readFileSync('./resources/svgs/veare-wordmark.svg')}
   <small class="Footer__copyright">Copyright ${new Date().getFullYear()} — Lukas Oppermann</small>
