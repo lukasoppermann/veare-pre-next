fd98ad02c3f5e536cbe1b20ac72f3224fcec62c1
Lukas Oppermann <lukas@vea.re>
2019-10-11 23:36:12 +0200

    prepare to be able to add lit footer to hbs files

--- app/routes/home.js
+++ app/routes/home.js
@@ -4,15 +4,18 @@ const fs = require('fs')
 const portfolioItems = JSON.parse(fs.readFileSync('resources/templates/data/portfolio.json'))
 const cache = require('../services/cacheService')()
 
-module.exports = (req, res) => {
+module.exports = (req, res, data) => {
   res.render('index', {
-    page: PageModel.findBySlug('home').fields,
-    pageClass: 'home',
-    projects: ProjectModel.all(),
-    partial: req.query.partial,
-    portfolioItems: portfolioItems.map(item => {
-      item.src = cache.get('files').media[item.src]
-      return item
-    })
+    ...data,
+    ...{
+      page: PageModel.findBySlug('home').fields,
+      pageClass: 'home',
+      projects: ProjectModel.all(),
+      partial: req.query.partial,
+      portfolioItems: portfolioItems.map(item => {
+        item.src = cache.get('files').media[item.src]
+        return item
+      })
+    }
   })
 }
