26a5638be530c29d9b794ccd2a79057bcd5c0ebe
Lukas Oppermann <lukas@vea.re>
2019-10-11 23:36:12 +0200

    prepare to be able to add lit footer to hbs files

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,6 @@
+// import footer from '../../resources/litTemplates/partials/footer'
 const express = require('express')
+// const { renderToString } = require('@popeindustries/lit-html-server')
 const router = express.Router()
 const basicAuth = require('express-basic-auth')
 const contentfulConfig = require('../config/contentful.js')
@@ -7,10 +9,10 @@ const cache = require('../services/cacheService')()
 /* =================
 // Normal Routes
 ================= */
-router.get(/^\/fragment\/menu$/, (req, res) => {
+router.get(/^\/fragment\/menu$/, (_req, res) => {
   res.render('menu')
 })
-router.get(/^\/?$/, (req, res) => {
+router.get(/^\/?$/, (_req, res) => {
   res.render('progressive', {
     filesStringify: {
       js: JSON.stringify(cache.get('files').js),
@@ -18,11 +20,15 @@ router.get(/^\/?$/, (req, res) => {
     }
   })
 })
-router.get(/^\/home$/, require('./home'))
+router.get(/^\/home$/, async (req, res) => {
+  return require('./home')(req, res, {
+    // footer: await renderToString(footer)
+  })
+})
 router.use('/portfolio', require('./portfolio'))
 router.get('/privacy', (req, res) => require('./pages')(req, res, 'privacy'))
-router.get('/about', (req, res) => { res.redirect('/#about') })
-router.get('/contact', (req, res) => { res.redirect('/#contact') })
+router.get('/about', (_req, res) => { res.redirect('/#about') })
+router.get('/contact', (_req, res) => { res.redirect('/#contact') })
 router.get('/blog', require('./blog').index)
 // router.get('/blog', require('./blog'))
 router.get(/^\/blog\/([\w-]+)/, require('./blog').get)
