6ecbf718690f95aebf6d1e7e6ce82f1b24203c77
Lukas Oppermann <lukas@vea.re>
2019-11-03 14:44:33 +0100

    working on blog previews

--- resources/litTemplates/partials/article-preview.ts
+++ resources/litTemplates/partials/article-preview.ts
@@ -1,20 +1,20 @@
 const { html } = require('@popeindustries/lit-html-server')
 const { unsafeHTML } = require('@popeindustries/lit-html-server/directives/unsafe-html.js')
 export default (article) => html`
-<li class="o-article-preview o-article-preview--${article.fields.category.fields.slug}" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
-    <div class="o-article-preview__meta">
+<li class="Article__preview o-article-preview--${article.fields.category.fields.slug}" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
+    <div class="Article__preview__meta">
         <meta itemprop="author" content="${article.fields.author.name}" />
         <meta itemprop="publisher" content="vea.re" />
-        <time itemprop="datePublished" class="o-article-preview__date" datetime="${article.fields.date}">${article.fields.date}</time>
+        <time itemprop="datePublished" class="Article__preview__date" datetime="${article.fields.date}">${article.fields.date}</time>
         ${!article.fields.category.fields.title ? ''
-          : unsafeHTML('<a href="" class="o-article-preview__category o-article-preview__category--' + article.fields.category.fields.slug + '">' + article.fields.category.fields.title + '</a>')
+          : unsafeHTML('<a href="" class="Article__preview__category Article__preview__category--' + article.fields.category.fields.slug + '">' + article.fields.category.fields.title + '</a>')
         }
-        <time class="o-article-preview__read-time" datetime="${article.fields.readingTime}m">${article.fields.readingTime} min read</time>
+        <time class="Article__preview__read-time" datetime="${article.fields.readingTime}m">${article.fields.readingTime} min read</time>
     </div>
-    <a itemprop="url" class="o-article-preview__link" title="Read: ${article.fields.title}" href="/blog/${article.fields.slug}">
+    <a itemprop="url" class="Article__preview__link" title="Read: ${article.fields.title}" href="/blog/${article.fields.slug}">
     <link itemprop="mainEntityOfPage" href="${article.fields.slug}" />
-        <h3 class="o-article-preview__title" itemprop="name headline">${article.fields.title}</h3>
-        <p class="o-article-preview__excerpt" itemprop="description">${article.fields.preview}</p>
+        <h3 class="Article__preview__title" itemprop="name headline">${article.fields.title}</h3>
+        <p class="Article__preview__excerpt" itemprop="description">${article.fields.preview}</p>
     </a>
 </li>
 `
