30139ba4c81272bcec2205a12164af8bcfd67706
Lukas Oppermann <oppermann.lukas@gmail.com>
2019-11-23 15:19:38 +0100

    Replace lazy loading (#270)
    * fix
    
    * working on native lazy loading
    
    * moved to native lazy loading and removed lazy-picture web component

--- resources/js/layoutComponents.ts
+++ resources/js/layoutComponents.ts
@@ -1,5 +1,4 @@
 import '../../node_modules/@nuclei-components/page-sections/dist/page-sections.js'
-import '../../node_modules/@nuclei-components/lazy-picture/dist/lazyPicture.js'
 import webComponentsSupported from './modules/webComponentsSupported'
 
 window.webComponentsSupported = webComponentsSupported
