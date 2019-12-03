c47f06aacedb879de94a6c3e02c7771a7d2e21e8
Lukas Oppermann <lukas@vea.re>
2019-12-03 07:20:44 +0100

    use projectInfo from project & add TOC

--- app/transformer/ProjectTransformer.ts
+++ app/transformer/ProjectTransformer.ts
@@ -1,4 +1,5 @@
 'use strict'
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
 
 const Transformer = require('./Transformer')
 const ChapterTransformer = require('./ChapterTransformer')
@@ -15,10 +16,14 @@ class ProjectTransformer extends Transformer {
         type: data.sys.contentType.sys.id,
         title: this.getContent(data, 'title'),
         subtitle: this.getContent(data, 'subtitle'),
-        titleStyle: this.getContent(data, 'titleStyle', 'default'),
         slug: this.getContent(data, 'slug'),
+        durationStart: this.getContent(data, 'durationStart'),
+        durationEnd: this.getContent(data, 'durationEnd'),
+        year: new Date(this.getContent(data, 'durationStart')).getFullYear(),
+        client: this.getContent(data, 'client'),
+        challenge: documentToHtmlString(this.getContent(data, 'challenge')),
+        roleAndTeam: documentToHtmlString(this.getContent(data, 'roleAndTeam')),
         header: new PictureElementTransformer(this.getContent(data, 'header')).first(),
-        headerImage: new AssetTransformer(this.getContent(data, 'headerImage')).first(),
         previewImage: new AssetTransformer(this.getContent(data, 'previewImage')).first(),
         chapters: new ChapterTransformer(this.getContent(data, 'chapters')).all(),
         variables: this.getContent(data, 'variables', []).reduce(
