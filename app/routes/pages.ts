2b9f12944de422252eed5873643782d7cf58344d
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-04-18 17:57:38 +0200

    working

--- app/routes/pages.ts
+++ app/routes/pages.ts
@@ -2,6 +2,7 @@ import page from '../templates/pages/page'
 const { renderToString } = require('@popeindustries/lit-html-server')
 const PageModel = require('../models/Page')()
 
-module.exports = async (_req, res, slug = 'home') => {
+module.exports = async (req, res) => {
+  const slug = 
   return res.send(await renderToString(page(PageModel.findBySlug(slug).fields)))
 }
