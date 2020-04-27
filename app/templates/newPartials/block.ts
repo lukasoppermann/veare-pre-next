530f81798abddcdbee7708287554e809424fc3b6
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-04-27 17:06:49 +0200

    fix for portfolio grid

--- app/templates/newPartials/block.ts
+++ app/templates/newPartials/block.ts
@@ -2,8 +2,8 @@ const { html } = require('@popeindustries/lit-html-server')
 const { unsafeHTML } = require('@popeindustries/lit-html-server/directives/unsafe-html.js')
 
 export default (fields) => html`
+${fields.slug !== null ? unsafeHTML('<a class="link__anchor" name="' + fields.slug + '"></a>') : ''}
 <div class="Block Grid ${fields.classes || ''}">
-  ${fields.slug !== null ? unsafeHTML('<a class="link__anchor" name="' + fields.slug + '"></a>') : ''}
   ${unsafeHTML(fields.content)}
 </div>
 `
