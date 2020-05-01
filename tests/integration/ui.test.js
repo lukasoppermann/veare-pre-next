30bafccdffbbe70d83025eb8d2546ab81b4800ab
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-05-01 22:19:39 +0200

    tests are running

--- tests/integration/ui.test.js
+++ tests/integration/ui.test.js
@@ -1,5 +1,29 @@
+const fs = require('fs')
 const puppeteer = require('puppeteer')
 const { toMatchImageSnapshot } = require('jest-image-snapshot')
+
+jest.setTimeout(30000);
+
+let browser
+let page
+const viewportHeight = 900
+const viewportWidth = 1680
+const testCases = [
+  ['/home', 8],
+  ['/work/nyon', 19],
+  ['/work/copra', 15],
+  ['/privacy', 16],
+  ['/blog', 2]
+]
+
+testCases.forEach(item => {
+  let dir = `${__dirname}/../screenshots${item[0]}`
+  if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir)
+  }
+})
+
+
 
 const browserConfig = {
   ignoreHTTPSErrors: true,
@@ -8,54 +32,78 @@ const browserConfig = {
 }
 
 // jest-image-snapshot custom configuration in order to save screenshots and compare the with the baseline
-function setConfig (filename) {
+function setConfig (filename, path) {
   return {
     failureThreshold: '0.01',
     failureThresholdType: 'percent',
-    customSnapshotsDir: `${__dirname}/../snapshots/`,
+    customSnapshotsDir: path,
     customSnapshotIdentifier: filename,
     noColors: true
   }
 }
 
-describe('screenshot', () => {
-  let browser;
-
-  beforeAll(async () => {
+beforeAll(async () => {
   // start Puppeteer with a custom configuration, see above the setup
   browser = await puppeteer.launch(browserConfig);
   expect.extend({ toMatchImageSnapshot })
+  page = await browser.newPage()
+  await page.setViewport({
+    width: viewportWidth,
+    height: viewportHeight,
+    deviceScaleFactor: 1
+  });
+});
+
+describe.each(testCases)('Testing: %s', (link, count) => {
+
+  beforeAll(async () => {
+    await page.goto(`http://localhost:3300${link}`)
+    await page.evaluate(() => {
+      window.scrollTo(0, Number.MAX_SAFE_INTEGER)
+    })
+    await page.waitFor(4000)
+    await page.evaluate(() => {
+      window.scrollBy(0, 0)
+    })
+    await page.waitFor(4000)
   });
 
-  it('dummy application page', async () => {
-    const page = await browser.newPage()
-    await page.setViewport({
-      width: 1680,
-      height: 900,
-      deviceScaleFactor: 1,
-    });
+  test.each(Array.from(Array(count), (_, i) => i))('Taking screenshot #%i', async i => {
+    let scrollHeight = i * viewportHeight
+    await page.evaluate(scrollHeight => {
+      window.scrollTo(0, scrollHeight)
+    }, scrollHeight)
+    let image = await page.screenshot({ path: `${__dirname}/../screenshots${link}/screenshot-${i}.png`})
+    expect(image).toMatchImageSnapshot(setConfig(`screenshot-${i}`, `${__dirname}/../screenshots${link}/snaps/`))
+  }, 15000)
 
-    await page.goto('http://localhost:3300/work/nyon')
-    await page.waitFor(4000)
-    const height = await page.evaluate(() => document.documentElement.offsetHeight)
-    const shotCount = 2 //Math.ceil(height/900)
-    console.debug('shotCount:',shotCount, height)
-    await page.evaluate(() => window.scrollTo(0, Number.MAX_SAFE_INTEGER/900));
-    for(let i = 0; i < shotCount; i++ ) {
-      await page.evaluate((i) => window.scrollTo(0, 900*i))
-      await page.waitFor(2000)
-      let image = await page.screenshot({ path: `${__dirname}/../snapshots/screenshot-${i}.png`, clip: {
-        x:0,
-        y: i*900,
-        width:1680,
-        height: 900
-      } })
-    }
-    // const image = await page.screenshot({ fullPage: false })
-    // expect(image).toMatchImageSnapshot(setConfig('image-name'))
-  }, 15000);
+})
 
-  afterAll(async () => {
-    await browser.close()
-  })
+// describe('/work/nyon', () => {
+//
+//   beforeAll(async () => {
+//     await page.goto('http://localhost:3300/work/nyon')
+//     await page.evaluate(() => {
+//       window.scrollTo(0, Number.MAX_SAFE_INTEGER)
+//     })
+//     await page.waitFor(4000)
+//     await page.evaluate(() => {
+//       window.scrollBy(0, 0)
+//     })
+//     await page.waitFor(4000)
+//   });
+//
+//   test.each(Array.from(Array(19), (_, i) => i))('Taking screenshot #%i', async i => {
+//     let scrollHeight = i * viewportHeight
+//     await page.evaluate(scrollHeight => {
+//       window.scrollTo(0, scrollHeight)
+//     }, scrollHeight)
+//     let image = await page.screenshot({ path: `${__dirname}/../screenshots/work-nyon-${i}.png`})
+//     expect(image).toMatchImageSnapshot(setConfig(`screenshot-${i}`))
+//   }, 15000)
+//
+// })
+
+afterAll(async () => {
+  await browser.close()
 })
