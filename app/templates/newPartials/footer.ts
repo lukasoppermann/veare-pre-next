507bfe77ebb637ef041f7a392e6cc3d705e03827
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-05-06 21:23:52 +0200

    working in footer

--- app/templates/newPartials/footer.ts
+++ app/templates/newPartials/footer.ts
@@ -6,9 +6,7 @@ export default html`
   <hr />
   <a class="veare-wordmark" href="/home" name="footer-home-link">${fs.readFileSync('./resources/svgs/veare-wordmark.svg')}</a>
   <div class="Footer__legal">
-    <a href="/privacy">Imprint</a>
-    <a href="/privacy">Privacy policy</a>
-    <small class="Footer__copyright">Copyright ${new Date().getFullYear()} — Lukas&nbsp;Oppermann</small>
+    <a href="/privacy">Imprint</a><a href="/privacy">Privacy policy</a><small class="Footer__copyright">Copyright ${new Date().getFullYear()} — Lukas&nbsp;Oppermann</small>
   </div>
 </footer>
 `
