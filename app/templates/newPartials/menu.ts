39578c6946e5187ecfa7f8740927de96cc7553bc
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-05-12 15:46:49 +0200

    working on menu

--- app/templates/newPartials/menu.ts
+++ app/templates/newPartials/menu.ts
@@ -2,11 +2,9 @@ const { html } = require('@popeindustries/lit-html-server')
 
 export default html`
   <menu class="Menu">
-    <svg class="Menu__icon" id="menuIcon" viewBox="0 0 800 500">
-      <path d="M270,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200" id="top"></path>
-      <path d="M270,320 L540,320" id="middle"></path>
-      <path d="M270,210 C300,210 520,210 540,210 C740,210 640,530 520,410 C440,330 300,190 300,190" id="bottom" transform="translate(480, 320) scale(1, -1) translate(-480, -318) "></path>
-    </svg>
+    <a class="Menu__icon">
+      Menu
+    </a>
     <nav class="responsive-menu__items">
       <a class="responsive-menu__item" href="/">Index</a>
       <a class="responsive-menu__item" href="/home#about">Resume</a>
--- resources/js/responsiveMenu.ts
+++ resources/js/responsiveMenu.ts
@@ -34,29 +34,29 @@ const responsiveMenu = menu => {
    */
   const _hideOverlay = () => {
     // hide overlay
-    menu.querySelector('#background').classList.remove('is-active')
+    // menu.querySelector('#background').classList.remove('is-active')
     menu.classList.remove('is-active')
 
-    setTimeout(() => {
-      menu.removeAttribute('overlayVisible')
-      document.body.style.overflow = 'auto'
-    }, 300)
+    // setTimeout(() => {
+    //   // menu.removeAttribute('overlayVisible')
+    //   document.body.style.overflow = 'auto'
+    // }, 300)
   }
   /**
    * @method toggleOverlay
    * @description everything tha happens when toggling the overlay
    */
-  const toggleOverlay = () => {
-    // show overlay
-    if (!menu.hasAttribute('overlayVisible')) {
-      document.body.style.overflow = 'hidden'
-      menu.querySelector('#background').classList.add('is-active')
-      menu.classList.add('is-active')
-      menu.setAttribute('overlayVisible', '')
-    } else {
-      _hideOverlay()
-    }
-  }
+  // const toggleOverlay = () => {
+  //   // show overlay
+  //   if (!menu.hasAttribute('overlayVisible')) {
+  //     document.body.style.overflow = 'hidden'
+  //     // menu.querySelector('#background').classList.add('is-active')
+  //     // menu.classList.add('is-active')
+  //     menu.setAttribute('overlayVisible', '')
+  //   } else {
+  //     _hideOverlay()
+  //   }
+  // }
   // hide / show on scroll
   let scrollEnded
   document.addEventListener('scroll', throttle(function () {
@@ -73,14 +73,14 @@ const responsiveMenu = menu => {
   }, 20))
   // add click event to menuIcon
   menu.querySelector('.Menu__icon').addEventListener('mouseover', () => {
-    console.debug('over')
     menu.classList.add('is-hovered')
   })
   menu.querySelector('.Menu__icon').addEventListener('mouseout', () => {
-    console.debug('out')
     menu.classList.remove('is-hovered')
   })
-  menu.querySelector('#menuIcon').addEventListener('click', toggleOverlay)
+  menu.querySelector('.Menu__icon').addEventListener('click', () => {
+    menu.classList.toggle('is-active')
+  })
   // close menu on link click
   menu.querySelectorAll('.responsive-menu__item').forEach(link => {
     link.addEventListener('click', (e) => {
