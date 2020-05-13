6869e491309a618fbb7688b64fdc4003a54a9eb9
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-05-13 14:50:07 +0200

    working on menu

--- app/templates/newPartials/menu.ts
+++ app/templates/newPartials/menu.ts
@@ -1,5 +1,6 @@
 const { html } = require('@popeindustries/lit-html-server')
 const { unsafeHTML } = require('@popeindustries/lit-html-server/directives/unsafe-html.js')
+const fs = require('fs')
 
 export default ($navItems: String) => html`
   <menu class="Menu GridNew">
@@ -11,6 +12,7 @@ export default ($navItems: String) => html`
     <a class="Menu__icon">
       Menu
     </a>
+    <a class="veare-wordmark" href="/home" name="footer-home-link">${fs.readFileSync('./resources/svgs/veare-wordmark.svg')}</a>
     <nav class="Menu__items">
       ${unsafeHTML($navItems)}
     </nav>
