6cd62dbb994b095c6d1300f2ee302c936775fc41
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-05-21 10:15:53 +0200

    working on new header

---
+++ app/templates/newPartials/header_intro.ts
@@ -0,0 +1,41 @@
+import cache from '../../services/cacheService'
+const { html } = require('@popeindustries/lit-html-server')
+const fs = require('fs')
+
+export default html`
+<header class="Header--intro Grid-32">
+
+  <div class="Header--intro__info">
+    <h5>Design</h5>
+    <p>— a toolkit of methods to explore and address user needs & problems.</p>
+  </div>
+  <div class="Header--intro__info">
+    <h5>Technology</h5>
+    <p>— the constraints but also the material from which to build an exciting solution.</p>
+  </div>
+  <div class="Header--intro__info">
+    <h5>Product Thinking</h5>
+    <p>— my approach of building products based on validated user needs & measured impact.</p>
+  </div>
+  <figure class="Header--intro__headline">
+    ${fs.readFileSync('./resources/svgs/solving-problems-is-my-passion.svg')}
+  </figure>
+  <h2 class="Header--intro__slogan">Designing experiences with a focus on usability</h2>
+  <div class="Header--intro__services">
+    <div class="Header--intro__services__description">
+      <h2 class="Header--intro__services__veare">veare</h2>
+      <span>supports clients with</span>
+    </div>
+    <h1 class="Header--intro__roles">
+      Design Lead UI / UX &<br />
+      Creative Direction
+    </h1>
+  </div>
+  <!-- <figure class="Header__Picture--Lukas-Oppermann">
+    <picture>
+      <img src="/${cache().get('files').media['media/lukas-oppermann@2x.png']}" alt="Lukas Oppermann" loading="lazy"/>
+    </picture>
+  </figure> -->
+  <div class="Header__background"></div>
+</header>
+`
--- app/templates/pages/progressive.ts
+++ app/templates/pages/progressive.ts
@@ -1,6 +1,6 @@
 import cacheService from '../../services/cacheService'
 import meta from '../meta'
-import headerIntro from '../partials/header_intro'
+import headerIntro from '../newPartials/header_intro'
 import menu from '../newPartials/menu'
 const { html } = require('@popeindustries/lit-html-server')
 const { unsafeHTML } = require('@popeindustries/lit-html-server/directives/unsafe-html.js')
@@ -15,29 +15,29 @@ export default () => html`
   <link rel="preconnect" href="http://images.ctfassets.net">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   ${meta('& Creative Direction – Lukas Oppermann', [
-        {
-          property: 'og:type',
-          value: 'website'
-        }, {
-          property: 'og:title',
-          value: 'Lukas Oppermann — Lead UI/UX Design & Creative Direction — vea.re'
-        }, {
-          property: 'og:description',
-          value: 'Lukas Oppermann is a design lead & creative director from berlin, germany. He loves creating experiences with a focus on usability.'
-        }, {
-          property: 'og:url',
-          value: 'https://vea.re'
-        }, {
-          property: 'og:image',
-          value: 'https://vea.re/media/veare-open-graph.jpg'
-        }, {
-          property: 'og:image:type',
-          value: 'image/jpeg'
-        }, {
-          property: 'og:image:alt',
-          value: 'Lukas Oppermann — Lead UI/UX Design & Creative Direction — vea.re'
-        }
-      ])}
+    {
+      property: 'og:type',
+      value: 'website'
+    }, {
+      property: 'og:title',
+      value: 'Lukas Oppermann — Lead UI/UX Design & Creative Direction — vea.re'
+    }, {
+      property: 'og:description',
+      value: 'Lukas Oppermann is a design lead & creative director from berlin, germany. He loves creating experiences with a focus on usability.'
+    }, {
+      property: 'og:url',
+      value: 'https://vea.re'
+    }, {
+      property: 'og:image',
+      value: 'https://vea.re/media/veare-open-graph.jpg'
+    }, {
+      property: 'og:image:type',
+      value: 'image/jpeg'
+    }, {
+      property: 'og:image:alt',
+      value: 'Lukas Oppermann — Lead UI/UX Design & Creative Direction — vea.re'
+    }
+  ])}
   <style>
     ${unsafeHTML(fs.readFileSync(path.resolve(__dirname, '../../../public/' + cache.get('files').css['css/slim.css'])))}
   </style>
