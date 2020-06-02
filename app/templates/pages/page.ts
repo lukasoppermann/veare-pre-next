c02e02b6028c3ae8df19ee76f4e42cb49e9db4a7
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-06-02 08:44:39 +0200

    remove Grid-32 class from privacy page

--- app/templates/pages/page.ts
+++ app/templates/pages/page.ts
@@ -4,9 +4,7 @@ const { html } = require('@popeindustries/lit-html-server')
 const { unsafeHTML } = require('@popeindustries/lit-html-server/directives/unsafe-html.js')
 
 export default (page, req): templateInterface => layout(html`
-  <section class="Grid-32">
-    ${unsafeHTML(page.content)}
-  </section>
+  ${unsafeHTML(page.content)}
 `, {
   bodyClass: page.slug,
   pageClass: `Page__${page.slug}`
