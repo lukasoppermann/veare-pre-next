c716a302a96e194f1d812384d28ab4e81ef05987
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-07-07 17:27:50 +0200

    working on adding details to project detail page

--- app/templates/pages/project.ts
+++ app/templates/pages/project.ts
@@ -11,7 +11,7 @@ export default (project, req) => {
     ${picture(project.header.fields, 'eager')}
   </header>
   <article>
-    <section class="Boxed-group">
+    <!-- <section class="Boxed-group">
       <div class="Boxed-item" style="flex-grow: 3">
         <h5>client</h5>
         <p>${project.client}</p>
@@ -25,13 +25,44 @@ export default (project, req) => {
         <h5>role</h5>
         ${unsafeHTML(project.roleAndTeam)}
       </div>
-    </section>
+    </section> -->
     <!-- {{!-- TOC --}} -->
     <section class="Project__intro">
       <ul class="Toc Project__toc" data-toc>
-      ${repeat(project.anchors, (anchor) => html`<li class="Toc__chapter"><a class="Toc__chapter__link" href="#${anchor}"><div class="Toc__chapter__title">${anchor.replace(/-/g, ' ')}</div></a></li>`)}
+        ${repeat(project.anchors, (anchor) => html`<li class="Toc__chapter"><a class="Toc__chapter__link" href="#${anchor}"><div class="Toc__chapter__title">${anchor.replace(/-/g, ' ')}</div></a></li>`)}
       </ul>
-      ${unsafeHTML(project.approach)}
+      <div class="Project__approach">
+        ${unsafeHTML(project.approach)}
+      </div>
+      <aside class="Project__info">
+        <!-- Year & Duration -->
+        <dl class="Project__info__time">
+          <dt><h6>Year</h6></dt>
+          <dd>
+            <time datetime="${project.years.start}">${project.years.start}</time>${
+              (project.years.start !== project.years.end) ? html` – <time datetime="${project.years.end}">${project.years.end}</time>` : ''}
+          </dd>
+          <dt><h6>Duration</h6></dt>
+          <dd><time datetime="${project.duration.totalWeeks * 4}W">
+            ${(project.duration.years > 0) ? html`${project.duration.years}&nbsp;yrs` : ''}
+            ${(project.duration.month > 0) ? html`${project.duration.month}&nbsp;mos` : ''}
+          </time></dd>
+        </dl>
+        <!-- Team -->
+        <div class="Project__info__team">
+          <h6>Team</h6>
+          <ul>
+            ${repeat(project.team, item => html`<li>${item}</li>`)}
+          </ul>
+        </div>
+        <!-- Responsibilities -->
+        <div class="Project__info__responsibilities">
+          <h6>Responsibilities</h6>
+          <ul>
+            ${repeat(project.responsibilities, item => html`<li>${item}</li>`)}
+          </ul>
+        </div>
+      </aside>
     </section>
     ${unsafeHTML(project.content)}
     <a class="Project__back_link" href="/home#portfolio">← Back</a>
