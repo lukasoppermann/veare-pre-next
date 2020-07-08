40887d23ed06601fcd2097587687ce4b1c9822f6
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-07-08 16:43:48 +0200

    working on adding details to project detail page (#407)
    * working on adding details to project detail page
    
    * remove commented section
    
    * remove commented section
    
    * working on updated title
    
    * adding project title above text

--- app/templates/pages/project.ts
+++ app/templates/pages/project.ts
@@ -7,31 +7,50 @@ const { unsafeHTML } = require('@popeindustries/lit-html-server/directives/unsaf
 export default (project, req) => {
   return layout(html`
   <header class="Header">
-    <h2 class="Project__title">${project.title}</h2>
     ${picture(project.header.fields, 'eager')}
   </header>
   <article>
-    <section class="Boxed-group">
-      <div class="Boxed-item" style="flex-grow: 3">
-        <h5>client</h5>
-        <p>${project.client}</p>
-      </div>
-      <div class="Boxed-item" style="flex-grow: 2">
-        <h5>year</h5>
-        <p><time datetime="${project.years.start}">${project.years.start}</time>${
-          (project.years.start !== project.years.end) ? html` – <time datetime="${project.years.end}">${project.years.end}</time>` : ''}<p>
-      </div>
-      <div class="Boxed-item" style="flex-grow: 4">
-        <h5>role</h5>
-        ${unsafeHTML(project.roleAndTeam)}
-      </div>
-    </section>
     <!-- {{!-- TOC --}} -->
     <section class="Project__intro">
-      <ul class="Toc Project__toc" data-toc>
-      ${repeat(project.anchors, (anchor) => html`<li class="Toc__chapter"><a class="Toc__chapter__link" href="#${anchor}"><div class="Toc__chapter__title">${anchor.replace(/-/g, ' ')}</div></a></li>`)}
+      <ul class="Toc Project__toc">
+        ${repeat(project.anchors, (anchor) => html`<li class="Toc__chapter"><a class="Toc__chapter__link" href="#${anchor}"><div class="Toc__chapter__title">${anchor.replace(/-/g, ' ')}</div></a></li>`)}
       </ul>
-      ${unsafeHTML(project.approach)}
+      <div class="Project__approach">
+        <div class="Project__title">
+          <h2 class="Project__title--client">${project.client}</h2>
+          <h1 class="Project__title--title">${project.subtitle}</h1>
+        </div>
+        ${unsafeHTML(project.approach)}
+      </div>
+      <aside class="Project__info">
+        <!-- Year & Duration -->
+        <dl class="Project__info__time">
+          <dt><h6>Year</h6></dt>
+          <dd>
+            <time datetime="${project.years.start}">${project.years.start}</time>${
+              (project.years.start !== project.years.end) ? html` – <time datetime="${project.years.end}">${project.years.end}</time>` : ''}
+          </dd>
+          <dt><h6>Duration</h6></dt>
+          <dd><time datetime="${project.duration.totalWeeks * 4}W">
+            ${(project.duration.years > 0) ? html`${project.duration.years}&nbsp;yrs` : ''}
+            ${(project.duration.month > 0) ? html`${project.duration.month}&nbsp;mos` : ''}
+          </time></dd>
+        </dl>
+        <!-- Team -->
+        <div class="Project__info__team">
+          <h6>Team</h6>
+          <ul>
+            ${repeat(project.team, item => html`<li>${item}</li>`)}
+          </ul>
+        </div>
+        <!-- Responsibilities -->
+        <div class="Project__info__responsibilities">
+          <h6>Responsibilities</h6>
+          <ul>
+            ${repeat(project.responsibilities, item => html`<li>${item}</li>`)}
+          </ul>
+        </div>
+      </aside>
     </section>
     ${unsafeHTML(project.content)}
     <a class="Project__back_link" href="/home#portfolio">← Back</a>
