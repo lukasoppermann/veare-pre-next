7a95df8e5714fba16b06c1d5693739f86d8ce6b0
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-07-09 13:10:51 +0200

    removed old portfolio items

--- app/templates/portfolio/portfolioItems.ts
+++
@@ -1,9 +0,0 @@
-import decks from './decks'
-import openEverything from './open-everything'
-import swift from './swift'
-
-export default {
-  decks: decks,
-  'open-everything': openEverything,
-  swift: swift
-}
