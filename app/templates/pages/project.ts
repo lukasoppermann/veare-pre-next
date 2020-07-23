91b17681eef1da57b689a06daad984c17e399cd1
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-07-23 10:58:29 +0200

    fixed usage of sources[0] to image

--- app/templates/pages/project.ts
+++ app/templates/pages/project.ts
@@ -10,7 +10,7 @@ export default (project, req) => {
     ${newPicture(project.header, 'eager', [
       {
         type: 'image/webp',
-        srcset: [400, 800, 1600, 3200].map(size => `${project.header.fields.sources[0].fields.url}?fm=webp&w=${size} ${size}w`).join(', '),
+        srcset: [400, 800, 1600, 3200].map(size => `${project.header.fields.image.fields.url}?fm=webp&w=${size} ${size}w`).join(', '),
         sizes: '(min-width: 1560px) 1540px, 100vw'
       }
     ])}
