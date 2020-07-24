ee27a14f1044e576e6e1dfd5256f365ebe32a42e
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-07-24 15:42:58 +0200

    add new picture element to homepage link template

--- app/templates/newPartials/link.ts
+++ app/templates/newPartials/link.ts
@@ -1,3 +1,4 @@
+import picture from '../newPartials/picture'
 const { html } = require('@popeindustries/lit-html-server')
 
 export default (link) => html`
@@ -7,19 +8,13 @@ export default (link) => html`
         <h4 class="Project-card__client">${link.subtitle}</h4>
         <h2 class="Project-card__title">${link.title}</h2>
       </div>
-      <figure class="Project-card__image-container">
-        <picture class="Project-card__image">
-        <source type="image/webp" srcset="${link.picture.fields.image.fields.url}?fm=webp&w=860" media="(min-width: 1200px)">
-        <source type="image/webp" srcset="${link.picture.fields.image.fields.url}?fm=webp&w=780" media="(min-width: 992px)">
-        <source type="image/webp" srcset="${link.picture.fields.image.fields.url}?fm=webp&w=930" media="(min-width: 768px)">
-        <source type="image/webp" srcset="${link.picture.fields.image.fields.url}?fm=webp&w=700" media="(min-width: 577px)">
-        <source type="image/webp" srcset="${link.picture.fields.image.fields.url}?fm=webp&w=500" media="(max-width: 576px)">
-        <img src="${link.picture.fields.image.fields.url}" alt="${link.picture.fields.title}" loading="lazy"/>
-        </picture>
-      </figure>
+      ${picture(link.picture.fields, 'lazy', [
+        {
+          type: 'image/webp',
+          srcset: [500, 1000, 1600, 2000].map(size => `${link.picture.fields.image.fields.url}?fm=webp&w=${size} ${size}w`).join(', '),
+          sizes: '(min-width: 760px) 930px, 100vw'
+        }
+      ])}
     </a>
   </section>
   `
-// PUT BACK TOP
-// <source type="image/webp" srcset="${link.picture.fields.image.fields.url}?fm=webp">
-// <img src="${link.picture.fields.image.fields.url}" alt="${link.picture.fields.title}" loading="lazy"/>
