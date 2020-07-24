e87c1c70aae9cb0349e0096ba5765aa66e7b4184
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-07-24 15:49:05 +0200

    prep using picture in excerpt

--- app/templates/newPartials/project_excerpt.ts
+++ app/templates/newPartials/project_excerpt.ts
@@ -7,8 +7,8 @@ export default (project) => html`
   <!-- Info -->
   <div class="Project-excerpt__info">
     <!-- PICTURE -->
-    <figure class="Project-excerpt__image-container Project-card__image-container">
-      <picture class="Project-card__image Project-excerpt__image">
+    <figure class="Project-excerpt__image-container">
+      <picture class="Project-excerpt__image">
         <source type="image/webp" srcset="${project.previewImage.fields.url}?fm=webp&w=1000" media="(min-width: 1200px)">
         <source type="image/webp" srcset="${project.previewImage.fields.url}?fm=webp&w=750" media="(min-width: 992px)">
         <source type="image/webp" srcset="${project.previewImage.fields.url}?fm=webp&w=930" media="(min-width: 768px)">
