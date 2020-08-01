c4d4d59269a3b87cf41ff2be1b0e2169eb50a864
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-08-01 15:41:34 +0200

    move middleware to app and remove express specific middleware in favor of agnostic plugins

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,13 @@
 import { revisionedFiles } from './services/files'
+import helmetSettings from './config/helmet-settings'
 const express = require('express')
 const fs = require('fs')
 const app = express()
+// middleware
+const compression = require('compression')
+const hemlet = require('helmet')
+const serveStatic = require('serve-static')
+const bodyParser = require('body-parser')
 
 export default async () => {
   // ---------------------------------- //
@@ -14,7 +20,10 @@ export default async () => {
   app.set('view cache', false) // should be enabled by default if process.env.NODE_ENV === "production"
   // ---------------------------------- //
   // MIDDLEWARE
-  app.use(require('./middleware'))
+  app.use(bodyParser.json({ type: 'application/*+json' }))
+  app.use(compression())
+  app.use(hemlet(helmetSettings))
+  app.use(serveStatic('public', { maxAge: '365d', etag: false }))
   // ---------------------------------- //
   // Dev Middleware
   if (process.env.NODE_ENV === 'development') {
