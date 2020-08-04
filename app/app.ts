c99e5122a8c96307278796bdaecaa5bf90d7be38
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-08-04 11:04:17 +0200

    use import for routes

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,6 @@
 import { revisionedFiles } from './services/files'
 import helmetSettings from './config/helmet-settings'
-// import routes from './routes/routes'
-const routes = require('./routes') // same error due to it trying to render footer before cache is set up
+import routes from './routes/routes'
 const express = require('express')
 const fs = require('fs')
 const app = express()
