b51c49a6b9fde96e7e488c177da937dc596574f9
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-08-06 09:33:24 +0200

    update deps

--- app/app.ts
+++ app/app.ts
@@ -2,9 +2,7 @@ import { revisionedFiles } from './services/files'
 import helmetSettings from './config/helmet-settings'
 import routes from './routes/routes'
 const connect = require('connect')
-// const express = require('express')
 const fs = require('fs')
-// const app = express()
 const app = connect()
 // middleware
 // const compression = require('compression')
@@ -16,11 +14,6 @@ export default () => {
   // ---------------------------------- //
   // load files into revisioned files
   revisionedFiles(JSON.parse(fs.readFileSync('public/rev-manifest.json', 'utf8')))
-  // disabled unneeded headers
-  // app.set('etag', false)
-  // app.set('x-powered-by', false)
-  // works with caching
-  // app.set('view cache', false) // should be enabled by default if process.env.NODE_ENV === "production"
   // ---------------------------------- //
   // MIDDLEWARE
   app.use(bodyParser.json({ type: 'application/*+json' }))
