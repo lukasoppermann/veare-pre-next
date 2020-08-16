c7bac821f7e35d2a119f82a1a530d21ec8328c57
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-08-16 09:51:47 +0200

    improve slugToUrl test

--- tests/unit/slugToUrlService.test.ts
+++ tests/unit/slugToUrlService.test.ts
@@ -1,11 +1,17 @@
 import slugToUrl from '../../app/services/slugToUrl'
 /* global test expect */
 describe("slugToUrl", () => {
-  test("testing slugToUrl service", () => {
+  test("testing slugToUrl service happy flow", () => {
     // assertion
     expect(slugToUrl('slug', 'project')).toStrictEqual('/work/slug')
     expect(slugToUrl('slug', 'page')).toStrictEqual('/slug')
     expect(slugToUrl('slug', 'article')).toStrictEqual('/blog/slug')
+  })
+
+  test("testing slugToUrl service leadingSlash false", () => {
+    // assertion
+    expect(slugToUrl('slug', 'project', true)).toStrictEqual('/work/slug')
+    expect(slugToUrl('slug', 'project', false)).toStrictEqual('work/slug')
   })
 
   test("testing slugToUrl service with wrong content type", () => {
