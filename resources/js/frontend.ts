b4a37f67cb9fa3688da156662c3361ee755c832a
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-08-24 15:03:16 +0200

    remove lithtml from frontend

--- resources/js/frontend.ts
+++ resources/js/frontend.ts
@@ -1,20 +1,15 @@
 /* global fetch */
 const app = window.app
 const homepage = fetch('/home?partial=true').then(response => response.text())
-// const menu = fetch('/fragment/menu').then(response => response.text())
 
-const litHtml = app.fetchInject([
-  `${app.baseUrl}/${app.files.js['js/litHtml.js']}`
-])
 const layout = app.fetchInject([
   `${app.baseUrl}/${app.files.css['css/app.css']}`
 ])
 
-Promise.all([homepage, litHtml, layout]).then(([homepageHtml]) => {
-  const content = (content) => app.html`${app.unsafeHTML(content)}`
+Promise.all([homepage, layout]).then(([homepageHtml]) => {
   const html = document.querySelector('.Page').innerHTML + ' ' + homepageHtml
   //
-  app.render(content(html), document.querySelector('.Page'))
+  document.querySelector('.Page').innerHTML = html
   document.querySelector('.Menu').classList.add('loaded')
 }).then(() => {
   app.fetchInject([
