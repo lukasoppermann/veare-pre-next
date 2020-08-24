7b01b8432be73dbea14b8dc1fc46c2009c007922
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-08-24 15:05:07 +0200

    cleanup page css

--- app/templates/pages/article.ts
+++ app/templates/pages/article.ts
@@ -8,31 +8,33 @@ const { unsafeHTML } = require('@popeindustries/lit-html-server/directives/unsaf
 const { repeat } = require('@popeindustries/lit-html-server/directives/repeat.js')
 // export template
 export default (article, req): templateInterface => layout(html`
-  <div class="Article">
-    <h1>${article.title}</h1>
-    <div class="Article__publication-details">
-      Last iteration <time class="Article__time" itemprop="datePublished" datetime="${article.rawLastIteration}">${article.lastIteration}</time>
-      •&nbsp;
-      <time class="Article__read-time" datetime="${article.readingTime}m">${article.readingTime} min read</time>
-    </div>
+  <div class="Page Page__blog" slug="${req.path}">
+    <div class="Article">
+      <h1>${article.title}</h1>
+      <div class="Article__publication-details">
+        Last iteration <time class="Article__time" itemprop="datePublished" datetime="${article.rawLastIteration}">${article.lastIteration}</time>
+        •&nbsp;
+        <time class="Article__read-time" datetime="${article.readingTime}m">${article.readingTime} min read</time>
+      </div>
 
-    <div class="Article__author">
-        — Published by <span itemprop="author" itemscope itemtype="http://schema.org/Person">
-          <a class="author" href="https://twitter.com/lukasoppermann" itemprop="url" title="Twitter account of Lukas Oppermann" rel="author">
-            <span itemprop="name">Lukas Oppermann</span></a></span> /
-        <span itemprop="publisher" itemtype="http://schema.org/Organization" itemscope>
-          <a itemprop="url" href="http://vea.re">
-            <span itemprop="name">vea.re</span>
-          </a>
-        </span>
-    </div>
+      <div class="Article__author">
+          — Published by <span itemprop="author" itemscope itemtype="http://schema.org/Person">
+            <a class="author" href="https://twitter.com/lukasoppermann" itemprop="url" title="Twitter account of Lukas Oppermann" rel="author">
+              <span itemprop="name">Lukas Oppermann</span></a></span> /
+          <span itemprop="publisher" itemtype="http://schema.org/Organization" itemscope>
+            <a itemprop="url" href="http://vea.re">
+              <span itemprop="name">vea.re</span>
+            </a>
+          </span>
+      </div>
 
-    ${unsafeHTML(article.content)}
-    <ul class="related-content">
-      ${repeat(article.relatedContent, (entry, index) => {
-        return html`<li class="related-content-item related-content-item--${index + 1}">${articlePreview(entry.fields)}</li>`
-      })}
-    </ul>
+      ${unsafeHTML(article.content)}
+      <ul class="related-content">
+        ${repeat(article.relatedContent, (entry, index) => {
+          return html`<li class="related-content-item related-content-item--${index + 1}">${articlePreview(entry.fields)}</li>`
+        })}
+      </ul>
+    </div>
   </div>
 `, {
   bodyClass: 'Page-Type__Article',
