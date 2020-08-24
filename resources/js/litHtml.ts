06ad357ca994dae27f41950a9275d9c17d271dc8
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-08-24 15:25:23 +0200

    remove lithtml from frontend (#440)

--- resources/js/litHtml.ts
+++
@@ -1,7 +0,0 @@
-
-import { html, render } from 'lit-html'
-import { unsafeHTML } from 'lit-html/directives/unsafe-html'
-
-window.app.html = html
-window.app.render = render
-window.app.unsafeHTML = unsafeHTML
