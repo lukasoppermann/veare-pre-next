7c81adbe78bc4555eb37aa9990a5f375ff2c8174
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-09-23 14:52:54 +0200

    added now to top menu

--- app/templates/newPartials/menu.ts
+++ app/templates/newPartials/menu.ts
@@ -5,6 +5,7 @@ const menuItems = activePath => html`
   <a href="/" class="Menu__link${activePath === '/' ? ' is-active' : ''}"><span class="Menu__link-text">Index</span></a>
   <a href="https://drive.google.com/open?id=10K9F9o0hokq4iPAbF5mseKJn_yflt19k" rel="noopener" target="_blank" class="Menu__link"><span class="Menu__link-text">Resume</span></a>
   <a href="/blog/" class="Menu__link${activePath === '/blog' ? ' is-active' : ''}"><span class="Menu__link-text">Writing</span></a>
+  <a href="/now/" class="Menu__link${activePath === '/now' ? ' is-active' : ''}"><span class="Menu__link-text">Now</span></a>
   <a class="Menu__link" target="_blank" rel="noopener" href="mailto:lukas@vea.re?subject=Hey 👋,%20what&apos;s%20up?&body=Great%20to%20hear%20from%20you,%20how%20can%20I%20help?"><span class="Menu__link-text">Contact</span></a>
 `
 
