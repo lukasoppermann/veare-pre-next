89797d4874743f2f87697c0d2e8e0a05acc943b2
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-10-20 23:51:02 +0200

    Setup shipit

---
+++ shipitfile.js
@@ -0,0 +1,44 @@
+// shipitfile.js
+module.exports = shipit => {
+  // Load shipit-deploy tasks
+  require('shipit-deploy')(shipit)
+
+  shipit.initConfig({
+    default: {
+      branch: 'master',
+      deployTo: '/home',
+      repositoryUrl: 'https://github.com/lukasoppermann/veare.git',
+      shallowClone: true,
+      keepReleases: 5,
+      deleteOnRollback: false,
+      ignores: [
+        '.git',
+        '.github',
+        'node_modules', 
+        'resources',
+        'logs', 
+        'tests'
+      ]
+    },
+    production: {
+      servers: 'root@vea.re'
+    }
+  })
+
+  // shipit.blTask('deploy:fetch', fetch)
+
+  shipit.on('published', async () => {
+    // copy .env config to server
+    await shipit.copyToRemote(
+      '.env',
+      `${shipit.releasePath}/.env`,
+    )
+    // install npm dependencies
+    await shipit.remote(`cd ${shipit.releasePath} && npm i --production`)
+  })
+
+  shipit.on('deployed', async () => {
+    // restart server
+    await shipit.remote(`cd ${shipit.releasePath} && npm run restart-server`)
+  })
+}
