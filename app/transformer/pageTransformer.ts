47660ba9a3dd4c758de016b55558b247aaf4377a
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-10-24 22:03:23 +0200

    working on introducing embeddedBlocks to page type

--- app/transformer/pageTransformer.ts
+++ app/transformer/pageTransformer.ts
@@ -1,10 +1,26 @@
 import { transformedPageFields } from '../../types/transformer'
 import transformer, { getField } from './transformer'
 import richText from '../services/convertRichText'
-
+/**
+ * @function getEmbeddedBlocks
+ * @param {array} content
+ */
+const getEmbeddedBlocks = (content: any[]) => {
+  return content
+    .filter(item => item.nodeType === 'embedded-entry-block' &&
+      ['block', 'picture'].includes(item.data.target.sys.contentType.sys.id)
+    ).map(item => ({
+      contentType: item.data.target.sys.contentType.sys.id,
+      id: item.data.target.sys.id
+    }))
+}
+/**
+ * Page Transformer
+ */
 export default async (data) => {
   return transformer(data, async (data) => {
     // transform richText
+
     const content = await richText(getField(data, 'content'))
     // return format
     return <transformedPageFields>{
@@ -13,7 +29,8 @@ export default async (data) => {
       rawLastIteration: getField(data, 'lastIteration'),
       lastIteration: new Date(getField(data, 'lastIteration')).toLocaleDateString('en-US', { month: 'short', year: 'numeric', day: 'numeric' }),
       description: getField(data, 'description'),
-      content: content.html
+      content: content.html,
+      embeddedBlocks: getEmbeddedBlocks(getField(data, 'content').content)
     }
   })
 }
