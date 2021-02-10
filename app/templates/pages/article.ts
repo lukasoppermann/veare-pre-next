3220215aa33a94c34531d8ac81c46586faa8640b
Lukas Oppermann <oppermann.lukas@gmail.com>
2021-02-10 15:44:57 +0100

    integrate mailchimp form

--- app/templates/pages/article.ts
+++ app/templates/pages/article.ts
@@ -37,13 +37,12 @@ export default (article: transformedArticleFields, req: requestInterface): templ
         <!-- Begin Mailchimp Signup Form -->
         <div id="mc_embed_signup">
         <form action="https://vea.us1.list-manage.com/subscribe/post?u=ccfc09fac7c56ed9104545178&amp;id=061df8693e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-            <div id="mc_embed_signup_scroll">
-          <label for="mce-EMAIL">Sign up to get informed about new posts &amp; projects.</label>
-          <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+          <div id="mc_embed_signup_scroll">
+            <label for="mce-EMAIL">Sign up to get informed about new posts &amp; projects.</label>
             <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_ccfc09fac7c56ed9104545178_061df8693e" tabindex="-1" value=""></div>
-            <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-            </div>
+            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+            <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+          </div>
         </form>
         </div>
         <!--End mc_embed_signup-->
